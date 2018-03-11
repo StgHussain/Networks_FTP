4fb7468575d950e0e0f700c5cc8597c5dc8e4567
TalhaGH <thussain1123@gmail.com>
2018-03-11 18:25:19 +0200

    1.1

--- Networks_FTP/server-ftp-thread.py
+++ Networks_FTP/server-ftp-thread.py
@@ -1,10 +1,12 @@
 import socket
 import threading
+import os
 import telnetlib
 
 
 Server_IP = socket.gethostbyname(socket.gethostname())
 Server_Port = 8000
+currdir = os.path.abspath('.')
 
 class FTPserverThread (threading.Thread):
     user = 'test user'
@@ -21,9 +23,7 @@ class FTPserverThread (threading.Thread):
     def run(self):
         self.connection.send('220 Welcome!\r\n')
         while True:
-            #cmd = self.connection.recv(256)   #not sure what these do yet
-            #if not cmd: break
-            #else
+            cmd = self.connection.recv(256)
 
     def SYST(self):
         self.connection.send('221 Linux')#change this to whatever system the actual server will be
@@ -34,20 +34,47 @@ class FTPserverThread (threading.Thread):
             self.connection.send('430 No user')
     def PASS(self, passwd):
         if passwd == password:
-            self.connection.send('230 OK \n')
+            self.connection.send('332 OK \n')
         else:
             self.connection.send('331 Incorrect password \n')
     def QUIT(self):
         self.connection.send('221 Goodbye \n')
     def NOOP(self):
         self.connection.send('200 OK \n')
-    def ACCFT(self):
-        self.connection.send()
-    def CWD(self, ):
-
+    def CWD(self, cmd ):
+        check_Path = cmd.os.path.exists
+        if check_Path is True:
+            os.chdir(cmd)
+            self.connection.send('250 OK \n')
+        else:
+            self.connection.send('550 No such file, directory \n')
     def CDUP(self, ):
+        if not os.path.samefile(self.cwd, self.basewd):
+            self.cwd = os.path.abs(os.path.join(self.cwd, '..'))
+        self.connection.send('200 OK \n')
 
     def MOUNT(self, ):
+
+    def PWD(self, cmd):
+        cwd = os.path.relpath(self.cmd,self.basewd)
+        if cwd=='.':
+            cwd='/'
+        else:
+            cwd='/'+ cwd
+        self.connnection.send('257 \"%s\"\r\n'%cwd)
+
+    def PORT(self, cmd):
+        if self.pasv_mode:
+            self.servsock
+            self.pasv_mode = False
+
+    def PASV(self, cmd):
+        self.pasv_mode = True
+        self.servsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.servesock.bind((Server_IP,0))
+        self.servsock.listen(1)
+        ip, port = self.servsock
+
 
 
 class FTPserver(threading.Thread):
@@ -65,13 +92,13 @@ class FTPserver(threading.Thread):
     def stop(self):
         self.socket.close()
 
-    if __name__ == '__main__':
-        ftp  = FTPserver()
-        ftp.daemon = True
-        ftp.start()
-        print 'Connection information', Server_IP, ':', Server_Port
-        raw_input('Enter to end...\n')
-        ftp.stop()
+if __name__ == '__main__':
+    ftp  = FTPserver()
+    ftp.daemon = True
+    ftp.start()
+    print 'Connection information', Server_IP, ':', Server_Port
+    raw_input('Enter to end...\n')
+    ftp.stop()
 
 
 
